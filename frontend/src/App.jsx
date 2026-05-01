062743ca92926a4ee5d5c7d88ae431eebfe489e4
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-05-01 15:44:36 +0700

    reset font end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,81 +1,9 @@
-// src/App.jsx
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home"; // Import component Home
-// import TopRates from "./pages/TopRates";
-// import MensWear from "./pages/MensWear";
-// import WomensWear from "./pages/WomensWear";
-// import KidsWear from "./pages/KidsWear";
-// // import Electronics from "./pages/Electronics";
-// import Perfume from "./pages/Perfume";
-// import Watch from "./pages/Watch";
-// import EssentialOil from "./pages/EssentialOil";
-import Login from "./components/Login/Login";
-import Register from "./components/Login/Register";
-import Footer from "./components/Footer/Footer";
-import Popup from "./components/Popup/Popup";
-import ForgotPassword from "./components/Login/ForgotPassword";
-import Category from "./pages/Category";
+import React from 'react'
 
 const App = () => {
-  const [orderPopup, setOrderPopup] = useState(false);
-  const handleOrderPopup = () => setOrderPopup((prev) => !prev);
-
-  useEffect(() => {
-    AOS.init({
-      offset: 100,
-      duration: 800,
-      easing: "ease-in-sine",
-      delay: 100,
-    });
-  }, []);
-
   return (
-    <BrowserRouter>
-      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
-        {/* Thanh Navbar */}
-        <Navbar />
+    <div>App</div>
+  )
+}
 
-        {/* Điều hướng giữa các trang */}
-        <Routes>
-          <Route
-            path="/"
-            element={<Home handleOrderPopup={handleOrderPopup} />}
-          />
-          {/* <Route path="/toprates" element={<TopRates />} /> */}
-          <Route
-            path="/mens"
-            element={<Category category="men" />}
-          />
-          <Route
-            path="/womens"
-            element={<Category category="women"/>}
-          />
-          <Route
-            path="/kids"
-            element={<Category category="kid"/>}
-          />
-          {/* <Route path="/electronic" element={<Electronics />} />
-          <Route path="/perfume" element={<Perfume />} />
-          <Route path="/watch" element={<Watch />} /> */}
-          {/* <Route path="/essentialOil" element={<EssentialOil />} /> */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />{" "}
-          {/* Đặt ngoài Login */}
-          <Route path="/register" element={<Register />} />
-        </Routes>
-
-        {/* Các thành phần khác */}
-        <Footer />
-        <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
-        {/* <Login useLogin={useLogin} setUseLogin={setUseLogin} />
-        <Register useRegister={useRegister} setUseRegister={setUseRegister} /> */}
-      </div>
-    </BrowserRouter>
-  );
-};
-
-export default App;
+export default App
